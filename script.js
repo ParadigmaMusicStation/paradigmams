1d0e65bc1c99fea6475fe96112982b1f58efd75c
ParadigmaMusicStation <tienda.paradigmams@gmail.com>
2026-03-31 16:48:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Este código asegura que la página cargue completamente antes de ejecutar funciones.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("¡Tu Linktree personalizado ha cargado correctamente!");
+    
+    // Aquí puedes agregar funciones a futuro. Por ejemplo, un botón 
+    // para alternar entre "Modo Claro" y "Modo Oscuro".
+});
